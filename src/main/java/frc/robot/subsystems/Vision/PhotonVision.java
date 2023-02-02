080d65a2cec1edd88d8c376a450e78c6c50b542c
Bizaare <xxthetacobell1o1xx@gmail.com>
2023-02-02 16:18:53 -0600

    camera update

--- src/main/java/frc/robot/subsystems/Vision/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/Vision/PhotonVision.java
@@ -1,28 +1,23 @@
 package frc.robot.subsystems.Vision;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.util.Units;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.Constants;
-import frc.robot.RobotContainer;
-import frc.robot.Constants.camConstants;
-
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.EstimatedRobotPose;
-
 import java.util.Optional;
 
-import org.ejml.equation.Variable;
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import edu.wpi.first.apriltag.AprilTagPoseEstimate;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.common.dataflow.structures.Packet;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.camConstants;
+import frc.robot.RobotContainer;
 
 public class PhotonVision {
+
+  int size;
+  byte[] packetData;
 
   // NetworkTable PhotonVision =
   // NetworkTableInstance.getDefault().getTable("photonvision"); // not in use
@@ -32,7 +27,19 @@ public class PhotonVision {
   private Optional<EstimatedRobotPose> poseOnField = Optional.empty();
   private  Optional<EstimatedRobotPose> getPose;
 
+
   private Boolean _hasPose = false;
+  public void Packet(int size) {
+    this.size = size;
+    packetData = new byte[size];
+  }
+
+/**
+ * Constructs a packet with the given data.
+ *
+ * @param data The packet data.
+ * @return 
+ */
 
   // private NetworkTableEntry result = PhotonVision.getEntry("PipelineIndex");
   // double photonresult = result.getDouble(0);
@@ -56,6 +63,8 @@ public class PhotonVision {
 
   public void PoseEstimating() {
 
+
+    // tell the robot when it sees an april tag
     if (_hasPose == true){
       poseOnField = this.getPose;
     } else if (_hasPose == false) {
@@ -66,9 +75,19 @@ public class PhotonVision {
     
     SmartDashboard.putBoolean("Has Pose", HasPose());
     this.getPose = estimatePose.update();
-
+    
   }
 
+
+  public void Packet(byte[] data) {
+    packetData = data;
+    size = packetData.length;
+  }
+  
+  /** Clears the packet and resets the read and write positions. */
+
+
+  // See if the robot is looking at an april tag
   public Boolean HasPose(){
     if(this.getPose == null) return false;
    if (this.getPose.isPresent()){
@@ -78,5 +97,19 @@ public class PhotonVision {
     }
 
     return _hasPose;
+  }
+
+  PhotonPipelineResult camResults = new PhotonPipelineResult();
+
+
+  public void clearCache() {
+    byte[] packetData = new byte[size];
+    int readPos = 0;
+    int writePos = 0;
+  }
+
+  public PhotonPipelineResult getCurrentResults() {
+ return null; 
+    
   }
 }
