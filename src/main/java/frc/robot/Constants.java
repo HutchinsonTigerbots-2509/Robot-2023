b46729e353bc83fefd8c619ffaace4c6b4d17c8c
Bizaare <xxthetacobell1o1xx@gmail.com>
2023-02-24 19:51:46 -0600

    more fixes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,10 +31,10 @@ public final class Constants {
     public static final int kFrontRightID = 1;
     public static final int kRearLeftID = 15;
     public static final int kRearRightID = 0;
-    public static final double kMaxSpeed = 0.8;
-    public static final double kMaxAngularSpeed = 0.8;
-    public static final double kHighSpeedStrafe = 1;
-    public static final double kLowSpeedStrafe = .6;
+    public static final double kMaxSpeed = 0.5;
+    public static final double kMaxAngularSpeed = 0.5;
+    public static final double kHighSpeedStrafe = 0.8;
+    public static final double kLowSpeedStrafe = .5;
     public static final double kSkewRateLimit = 0.8;
     public static final double kGearRatio = 12.0;
     public static final int kFalconUnitsPerRotation = 2048;
